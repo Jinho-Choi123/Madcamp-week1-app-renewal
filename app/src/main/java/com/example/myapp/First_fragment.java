a1f8db0114c6a3e64ca3a10066d4326367dcb8cf
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-01 14:19:32 +0900

    fix:problem of fragment name attribute

--- app/src/main/java/com/example/myapp/First_fragment.java
+++ app/src/main/java/com/example/myapp/First_fragment.java
@@ -32,8 +32,7 @@ public class First_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_first_fragment, null);
+        View view = inflater.inflate(R.layout.fragment_first_fragment, container, false);
 
         ArrayList<Contact> contacts = Contact.read(getContext());
 
@@ -48,6 +47,8 @@ public class First_fragment extends Fragment {
             iter = contacts.get(i);
             adapter.addItem(iter.getPhoneNumber(), iter.getName(), iter.getId());
         }
+
+
         adapter.notifyDataSetChanged();
 
         return view;
