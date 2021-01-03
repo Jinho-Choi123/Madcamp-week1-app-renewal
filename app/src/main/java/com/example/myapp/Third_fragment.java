7fa7f06c4d4a0a6b16b3d2c13bf4b7464f00b8b0
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-04 00:05:34 +0900

    fix:fixing bug

--- app/src/main/java/com/example/myapp/Third_fragment.java
+++ app/src/main/java/com/example/myapp/Third_fragment.java
@@ -1,63 +1,250 @@
 package com.example.myapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Third_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.util.ArrayList;
+
 public class Third_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int PICK_IMAGES = 2;
+    public static final int STORAGE_PERMISSION = 100;
+    private static final int PICK_FROM_CAMERA = 2;
+    ArrayList<ImageModel> imageList;
+    ArrayList<String> selectedImageList;
+    RecyclerView imageRecyclerView, selectedImageRecyclerView;
+    int[] resImg = {R.drawable.ic_camera_white_30dp, R.drawable.ic_folder_white_30dp};
+    String[] title = {"Camera", "Folder"};
+    String mCurrentPhotoPath;
+    com.example.myapp.SelectedImageAdapter selectedImageAdapter;
+    com.example.myapp.ImageAdapter imageAdapter;
+    String[] projection = {MediaStore.MediaColumns.DATA};
+    File image;
 
     public Third_fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Third_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Third_fragment newInstance(String param1, String param2) {
         Third_fragment fragment = new Third_fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_third_fragment, container, false);
+        if (isStoragePermissionGranted()) {
+            init(view);
+            getAllImages();
+            setImageList();
+            setSelectedImageList();
+        }
+        return view ;
+    }
+
+    public void init(View view) {
+        imageRecyclerView = view.findViewById(R.id.recycler_view);
+        selectedImageRecyclerView = view.findViewById(R.id.selected_recycler_view);
+        selectedImageList = new ArrayList<>();
+        imageList = new ArrayList<>();
+    }
+
+    public void setImageList() {
+        imageRecyclerView.setLayoutManager(new GridLayoutManager(getContext().getApplicationContext(), 4));
+        imageAdapter = new com.example.myapp.ImageAdapter(getContext().getApplicationContext(), imageList);
+        imageRecyclerView.setAdapter(imageAdapter);
+
+        imageAdapter.setOnItemClickListener(new com.example.myapp.ImageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                if (position == 0) {
+                    getPickImageIntent();
+                } else {
+                    try {
+                        if (!imageList.get(position).isSelected) {
+                            selectImage(position);
+                        } else {
+                            unSelectImage(position);
+                        }
+                    } catch (ArrayIndexOutOfBoundsException ed) {
+                        ed.printStackTrace();
+                    }
+                }
+            }
+        });
+        setImagePickerList();
+    }
+
+    public void setSelectedImageList() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        selectedImageRecyclerView.setLayoutManager(layoutManager);
+        selectedImageAdapter = new com.example.myapp.SelectedImageAdapter(getActivity(), selectedImageList);
+        selectedImageRecyclerView.setAdapter(selectedImageAdapter);
+    }
+
+    // Add Camera and Folder in ArrayList
+    public void setImagePickerList() {
+        for (int i = 1; i < resImg.length; i++) {
+            ImageModel imageModel = new ImageModel();
+            imageModel.setResImg(resImg[i]);
+            imageModel.setTitle(title[i]);
+            imageList.add(0, imageModel);
+        }
+        imageAdapter.notifyDataSetChanged();
+    }
+
+    // get all images from external storage
+    public void getAllImages() {
+        imageList.clear();
+        Cursor cursor = getContext().getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+        while (cursor.moveToNext()) {
+            String absolutePathOfImage = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+            ImageModel ImageModel = new ImageModel();
+            ImageModel.setImage(absolutePathOfImage);
+            imageList.add(ImageModel);
+        }
+        cursor.close();
+    }
+
+    public void getPickImageIntent() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent, PICK_IMAGES);
+    }
+
+    // Add image in selectedImageList
+    public void selectImage(int position) {
+        // Check before add new item in ArrayList;
+        if (!selectedImageList.contains(imageList.get(position).getImage())) {
+            imageList.get(position).setSelected(true);
+            selectedImageList.add(0, imageList.get(position).getImage());
+            selectedImageAdapter.notifyDataSetChanged();
+            imageAdapter.notifyDataSetChanged();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_fragment, container, false);
+    // Remove image from selectedImageList
+    public void unSelectImage(int position) {
+        for (int i = 0; i < selectedImageList.size(); i++) {
+            if (imageList.get(position).getImage() != null) {
+                if (selectedImageList.get(i).equals(imageList.get(position).getImage())) {
+                    imageList.get(position).setSelected(false);
+                    selectedImageList.remove(i);
+                    selectedImageAdapter.notifyDataSetChanged();
+                    imageAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                if (mCurrentPhotoPath != null) {
+                    addImage(mCurrentPhotoPath);
+                }
+            } else if (requestCode == PICK_IMAGES) {
+                if (data.getClipData() != null) {
+                    ClipData mClipData = data.getClipData();
+                    for (int i = 0; i < mClipData.getItemCount(); i++) {
+                        ClipData.Item item = mClipData.getItemAt(i);
+                        Uri uri = item.getUri();
+                        getImageFilePath(uri);
+                    }
+                } else if (data.getData() != null) {
+                    Uri uri = data.getData();
+                    getImageFilePath(uri);
+                }
+            }
+        }
+    }
+
+    // Get image file path
+    public void getImageFilePath(Uri uri) {
+        Cursor cursor = getContext().getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String absolutePathOfImage = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                if (absolutePathOfImage != null) {
+                    checkImage(absolutePathOfImage);
+                } else {
+                    checkImage(String.valueOf(uri));
+                }
+            }
+        }
+    }
+
+    public void checkImage(String filePath) {
+        // Check before adding a new image to ArrayList to avoid duplicate images
+        if (!selectedImageList.contains(filePath)) {
+            for (int pos = 0; pos < imageList.size(); pos++) {
+                if (imageList.get(pos).getImage() != null) {
+                    if (imageList.get(pos).getImage().equalsIgnoreCase(filePath)) {
+                        imageList.remove(pos);
+                    }
+                }
+            }
+            addImage(filePath);
+        }
+    }
+
+    // add image in selectedImageList and imageList
+    public void addImage(String filePath) {
+        ImageModel imageModel = new ImageModel();
+        imageModel.setImage(filePath);
+        imageModel.setSelected(true);
+        imageList.add(2, imageModel);
+        selectedImageList.add(0, filePath);
+        selectedImageAdapter.notifyDataSetChanged();
+        imageAdapter.notifyDataSetChanged();
+    }
+
+    public boolean isStoragePermissionGranted() {
+        int ACCESS_EXTERNAL_STORAGE = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if ((ACCESS_EXTERNAL_STORAGE != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+            return false;
+        }
+        return true;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //init();
+            getAllImages();
+            setImageList();
+            setSelectedImageList();
+        }
     }
 }
